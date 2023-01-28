1f1a56d709172ddaaa81e5436547addc0cc392bc
dan <villalbautria08@gmail.com>
2023-01-28 12:04:52 -0500

    Swagger docs

--- api/app.ts
+++ api/app.ts
@@ -1,8 +1,11 @@
 import fastifyAutoload from "@fastify/autoload";
 import fastifyMongodb from "@fastify/mongodb";
 import fastifySensible from "@fastify/sensible";
+import { fastifySwagger } from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
+import pkg from "./package.json" assert { type: "json" };
 import fp from "./utils/fp.js";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -12,6 +15,20 @@ export default fp(async (app, opts) => {
   app.register(fastifyMongodb, {
     forceClose: true,
     url: process.env["DB_URL"]!,
+  });
+  app.register(fastifySwagger, {
+    openapi: {
+      info: {
+        title: "API",
+        version: pkg.version,
+      },
+    },
+  });
+  app.register(fastifySwaggerUi, {
+    routePrefix: "/",
+    uiConfig: {
+      deepLinking: true,
+    },
   });
   app.register(fastifySensible);
   app.register(fastifyAutoload, {
--- api/utils/model.ts
+++ api/utils/model.ts
@@ -5,8 +5,17 @@ export const BaseModel = z.object({
   _id: z
     .string()
     .length(8)
-    .default(() => nanoid(8)),
-  created: z.number().default(() => Date.now()),
-  updated: z.number().default(() => Date.now()),
+    .default(() => nanoid(8))
+    .optional(),
+  created: z
+    .string()
+    .datetime()
+    .default(() => new Date().toISOString())
+    .optional(),
+  updated: z
+    .string()
+    .datetime()
+    .default(() => new Date().toISOString())
+    .optional(),
   deleted: z.number().optional(),
 });
