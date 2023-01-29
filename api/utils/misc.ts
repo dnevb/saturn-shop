055a6448e7a00ae88b23f32634ad710915fbf4df
dan <villalbautria08@gmail.com>
2023-01-29 11:53:57 -0500

    GET routes utils

---
+++ api/utils/misc.ts
@@ -0,0 +1,72 @@
+import { noCase } from "change-case";
+import type { FastifyInstance } from "fastify";
+import { z, ZodObject, ZodRawShape } from "zod";
+import { zodToJsonSchema } from "zod-to-json-schema";
+import fp from "./fp.js";
+
+const searchSchema = z
+  .object({
+    query: z.string(),
+    processingTimeMs: z.number(),
+    limit: z.number(),
+    offset: z.number(),
+    totalPages: z.number(),
+    estimatedTotalHits: z.number(),
+    totalHits: z.number(),
+  })
+  .partial();
+
+export const createGetRoute = <T extends ZodRawShape>(
+  app: FastifyInstance,
+  index: string,
+  schema: ZodObject<T>
+) =>
+  app.route<{ Params: { id: string } }>({
+    method: "GET",
+    url: "/:id",
+    schema: {
+      tags: [noCase(index)],
+      params: { id: { type: "string" } },
+      response: { 200: zodToJsonSchema(schema) },
+    },
+    handler: async (req) => {
+      const col = app.mongo.db!.collection(index);
+
+      const res = await col.findOne({ _id: req.params["id"] });
+
+      return res || app.httpErrors.notFound();
+    },
+  });
+
+export const createFindRoute = <T extends ZodRawShape>(
+  app: FastifyInstance,
+  index: string,
+  schema: ZodObject<T>
+) =>
+  app.route<{ Querystring: { q: string; filter: string } }>({
+    method: "GET",
+    url: "",
+    schema: {
+      tags: [noCase(index)],
+      querystring: { q: { type: "string" }, filter: { type: "string" } },
+      response: {
+        200: zodToJsonSchema(
+          searchSchema.extend({ hits: schema.array() })
+        ),
+      },
+    },
+    handler: async (req) => {
+      const col = app.search.index(index);
+
+      return col.search(req.query["q"], { filter: req.query["filter"] });
+    },
+  });
+
+export const createGetRoutes = <T extends ZodRawShape>(
+  index: string,
+  schema: ZodObject<T>
+) =>
+  fp(async (app) => {
+    createGetRoute(app, index, schema);
+    createFindRoute(app, index, schema);
+  });
