881821cb8f38c480b6c302d67808c3e1aed6c330
dan <villalbautria08@gmail.com>
2023-01-31 11:50:21 -0500

    Colors, Styles & favicon

--- site/uno.config.ts
+++ site/uno.config.ts
@@ -2,4 +2,15 @@ import { defineConfig, presetUno } from "unocss";
 
 export default defineConfig({
   presets: [presetUno()],
+  theme: {
+    colors: {
+      brand: {
+        lighter: "#aaa0f8",
+        light: "#8983f5",
+        DEFAULT: "#6366f1",
+        dark: "#5151bc",
+        darker: "#3f3d89",
+      },
+    },
+  },
 });
